55a6c3e2fe79ae0ecfbe5f0898527f85e7ba3234
louai drissi <louai2004@gmail.com>
2017-12-31 02:29:54 -0500

    adding pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { convertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    convertToSpacesPipe
   ],
   imports: [
     BrowserModule,
